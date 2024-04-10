1f0f69ae34acfd4558157745deaf55378666281f
komeno <tkomeno@student.42tokyo.jp>
2024-04-10 17:49:01 +0900

    Feat: phonebook rewrite. Still developing

--- cpp-module00/ex01/phonebook.cpp
+++ cpp-module00/ex01/phonebook.cpp
@@ -1,76 +1,35 @@
 #include <iostream>
-#include <iomanip>
 
-std::string promptUser(std::string prompt) {
-	std::string result;
+#define PROMPT_ERROR 1
+#define PROMPT_SUCCESS 0
 
-	std::cout << prompt;
-	std::cin >> result;
-
-	return (result);
+int prompt(std::string &input) {
+	std::cout << "phonebook> ";
+	if (!(std::cin >> input)) {
+		if (std::cin.eof()) {
+			std::cout << "EOF" << std::endl;
+			return (PROMPT_ERROR);
+		}
+	}
+	return (PROMPT_SUCCESS);
 }
 
-class Contact {
-public:
-	std::string firstName;
-	std::string lastName;
-	std::string nickname;
-	std::string phoneNumber;
-	std::string darkestSecret;
-};
-
-class Phonebook {
-private:
-	int		index;
-	Contact contacts[8];
-
-public:
-	Phonebook() : index(0) {}
-
-	void add() {
-		if (index == 8) {
-			index = 0;
-		}
-
-		contacts[index].firstName = promptUser("Enter first name: ");
-		contacts[index].lastName = promptUser("Enter last name: ");
-		contacts[index].nickname = promptUser("Enter nickname: ");
-		contacts[index].phoneNumber = promptUser("Enter phone number: ");
-		contacts[index].darkestSecret = promptUser("Enter darkest secret: ");
-		std::cout << "Successfully added contact!" << std::endl;
-
-		index++;
-	}
-
-	void search(void) {
-		
-		for (int i = 0; i < index; i++) {
-			std::cout << std::setw(10) << i << " | ";
-			std::cout << std::setw(10) << contacts[i].firstName << " | ";;
-			std::cout << std::setw(10) << contacts[i].lastName << " | ";;
-			std::cout << std::setw(10) << contacts[i].nickname << std::endl;
-		}
-	}
-};
-
 int main(void) {
-	Phonebook book;
-	std::string command;
+	std::string input;
 
 	while (true) {
-		command = promptUser("phonebook> ");
-
-		if (command == "ADD")
-			book.add();
-		else if (command == "SEARCH") {
-			book.search();
-		}
-		else if (command == "EXIT") {
-			std::cout << "Exit." << std::endl;
+		if (prompt(input) == PROMPT_ERROR) {
 			break ;
 		}
-		else {
-			std::cout << "Invalid command." << std::endl;
+		if (input == "ADD") {
+			std::cout << "Adding!" << std::endl;
+		}
+		if (input == "SEARCH") {
+			std::cout << "Searching!" << std::endl;
+		}
+		if (input == "EXIT") {
+			std::cout << "Exit" << std::endl;
+			break ;
 		}
 	}
 }
