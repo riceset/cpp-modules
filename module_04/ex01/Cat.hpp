930bad3916d562d7cff6a657487cc9209efc440f
komeno <tkomeno@student.42tokyo.jp>
2024-05-14 11:00:53 +0900

    Added Brain class member to other classes

--- module_04/ex01/Cat.hpp
+++ module_04/ex01/Cat.hpp
@@ -2,8 +2,10 @@
 #define CAT_HPP
 
 #include "Animal.hpp"
+#include "Brain.hpp"
 
 class Cat : public Animal {
+	Brain* brain;
 public:
 	Cat();
 	Cat(const Cat& other);
--- module_04/ex01/Dog.hpp
+++ module_04/ex01/Dog.hpp
@@ -2,8 +2,10 @@
 #define DOG_HPP
 
 #include "Animal.hpp"
+#include "Brain.hpp"
 
 class Dog : public Animal {
+	Brain* brain;
 public:
 	Dog();
 	Dog(const Dog& other);
