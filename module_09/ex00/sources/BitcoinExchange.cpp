17d8115b0b682635a338783883373620503cbf08
komeno <tkomeno@student.42tokyo.jp>
2024-10-11 08:23:54 +0000

    feat: Parsing files and throwing errors

--- module_09/ex00/sources/BitcoinExchange.cpp
+++ module_09/ex00/sources/BitcoinExchange.cpp
@@ -4,21 +4,34 @@ void skipHeader(std::ifstream& file, std::string& line) {
     std::getline(file, line);
 }
 
+void getlineCustom(std::stringstream& ss, std::string& result) {
+    result.clear();
+    char ch;
+    while (ss.get(ch)) {
+        if (ch == ',' || ch == '|') {
+            break;
+        }
+        result += ch;
+    }
+}
+
 void readRate(ExchangeRate& currentEntry, std::stringstream& ssLine) {
     std::string sRate;
-    std::getline(ssLine, sRate, ',');
+    getlineCustom(ssLine, sRate);
     std::stringstream ssRate(sRate);
     ssRate >> currentEntry.rate;
 }
 
 void readDate(ExchangeRate& currentEntry, std::stringstream& ssLine) {
-    std::getline(ssLine, currentEntry.date, ',');
+    getlineCustom(ssLine, currentEntry.date);
 }
 
 std::vector<ExchangeRate> readData(const std::string& filename) {
+    std::string sLine;
     std::vector<ExchangeRate> rates;
     std::ifstream file(filename.c_str());
-    std::string sLine;
+    if (!file.is_open())
+        throw std::runtime_error("Error: could not open file.");
 
     skipHeader(file, sLine);
     while (std::getline(file, sLine)) {
--- module_09/ex00/sources/main.cpp
+++ module_09/ex00/sources/main.cpp
@@ -1,12 +1,26 @@
 #include "BitcoinExchange.hpp"
-#include <iomanip> // Include this header for std::fixed and std::setprecision
+#include <iomanip>
 
-int main() {
-    std::vector<ExchangeRate> rates = readData("data.csv");
-    
+void printRates(const std::vector<ExchangeRate>& rates) {
     for (size_t i = 0; i < rates.size(); ++i) {
         std::cout << "Date: " << rates[i].date << ", Rate: " << std::fixed << std::setprecision(2) << rates[i].rate << std::endl;
     }
+}
 
-    return 0;
+std::string parseInfile(int argc, char **argv) {
+    if (argc != 2)
+        throw std::runtime_error("Error: could not open file.");
+    return (std::string(argv[1]));
+}
+
+int main(int argc, char **argv) {
+    try {
+        std::string filename = parseInfile(argc, argv);
+        std::vector<ExchangeRate> outfile = readData(filename);
+        std::vector<ExchangeRate> rates = readData("data.csv");
+        printRates(outfile);
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+    return (0);
 }
